853b5bddc421bcd7b2f5b4f37718d1fb80022c47
igor_eglit <igeglits@gmail.com>
2024-01-23 16:03:46 +0200

    добавлена музыка

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,14 +6,20 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridLayout;
 import android.widget.TextView;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaPlayer[] mediaPlayers;
     private int targetNumber;
     private TextView hintTextView;
+    private Button muteButton; // Добавлено поле для кнопки mute
+
+    private boolean isMuted = false; // Добавлено поле для отслеживания состояния mute
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +27,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         hintTextView = findViewById(R.id.hintTextView);
+        muteButton = findViewById(R.id.muteButton); // Инициализация кнопки mute
+
         generateTargetNumber();
+
+        // Идентификаторы ресурсов для ваших аудиофайлов в папке raw
+        int[] audioResourceIds = {R.raw.ambient_light, R.raw.bilateral_eternal, R.raw.bilateral_harp};
+        // Инициализация массива MediaPlayer
+        mediaPlayers = new MediaPlayer[audioResourceIds.length];
+        // Создание MediaPlayer для каждого аудиофайла
+        for (int i = 0; i < audioResourceIds.length; i++) {
+            mediaPlayers[i] = MediaPlayer.create(this, audioResourceIds[i]);
+            mediaPlayers[i].setLooping(true); // для зацикливания звука
+        }
+
+        // Воспроизведение первого аудиофайла
+        if (mediaPlayers.length > 0) {
+            mediaPlayers[0].start();
+        }
     }
 
     public void onNumberButtonClick(View view) {
@@ -36,6 +59,14 @@ public class MainActivity extends AppCompatActivity {
             // Если это не кнопка "info", обрабатываем как угадывание числа
             int enteredNumber = Integer.parseInt(buttonText);
             checkGuess(enteredNumber);
+        }
+    }
+
+    public void onMuteButtonClick(View view) {
+        if (isMuted) {
+            unmute(); // Включаем звук
+        } else {
+            mute(); // Выключаем звук
         }
     }
 
@@ -69,7 +100,38 @@ public class MainActivity extends AppCompatActivity {
                     hintTextView.animate().alpha(1f).setDuration(1000).start();
                 })
                 .start();
+    }
 
+    private void mute() {
+        stopMediaPlayers(); // Останавливаем все медиапроигрыватели
+        isMuted = true; // Устанавливаем состояние mute
+      //  muteButton.setBackgroundResource(R.drawable.ic_volume_off); // Устанавливаем иконку выключенного звука
+        Toast.makeText(this, "Sound Off", Toast.LENGTH_SHORT).show();
+    }
 
+    private void unmute() {
+        for (MediaPlayer mediaPlayer : mediaPlayers) {
+            if (mediaPlayer != null) {
+                mediaPlayer.start(); // Запускаем звук, если он был выключен
+            }
+        }
+        isMuted = false; // Устанавливаем состояние unmute
+       // muteButton.setBackgroundResource(R.drawable.ic_volume_up); // Устанавливаем иконку включенного звука
+        Toast.makeText(this, "Sound On", Toast.LENGTH_SHORT).show();
+    }
+
+    private void stopMediaPlayers() {
+        for (MediaPlayer mediaPlayer : mediaPlayers) {
+            if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopMediaPlayers();
     }
 }
